0c9a386a2ea0e59da28d096b849ea6262403735a
wangyafeng <276180638@qq.com>
2015-03-04 16:33:47 +0800

    修复：去掉页面多余的蓝色块

--- src/server/portal/shenqi/default/views/default/admin/user/userlog.php
+++ src/server/portal/shenqi/default/views/default/admin/user/userlog.php
@@ -1,8 +1,5 @@
 <div class="row">
     <div class="col-xs-12">
-        <div class="table-header">
-            &nbsp;
-        </div>
 
         <div class="table-responsive">
             <div class="dataTables_wrapper">   
