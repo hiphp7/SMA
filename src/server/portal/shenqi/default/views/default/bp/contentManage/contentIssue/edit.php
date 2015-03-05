21c108d9d6f3a9f2b5652776e6a2670ee361213d
wangyafeng <276180638@qq.com>
2015-03-05 15:15:21 +0800

    修正：去掉提示语多余字符

--- src/server/portal/shenqi/default/views/default/bp/contentManage/contentIssue/edit.php
+++ src/server/portal/shenqi/default/views/default/bp/contentManage/contentIssue/edit.php
@@ -99,7 +99,7 @@ $(function(){
 		    <div class="col-xs-12 col-sm-5">                        
                             <a href="/attachments/tpl/target.php">点击下载模板文件</a>
 		    </div>
-		    <div class="help-block col-xs-12 col-sm-reset inline">请先下载模板文件,模板为txt文件,请使用记事本编辑,utf-8编>码保存</div>                                                                      
+		    <div class="help-block col-xs-12 col-sm-reset inline">请先下载模板文件,模板为txt文件,请使用记事本编辑,utf-8编码保存</div>                                                                      
 		    </div>
 <div class="form-group">
                     <label class="col-xs-12 col-sm-2 control-label no-padding-right" >上传群发号码</label>
